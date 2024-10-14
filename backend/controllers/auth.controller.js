cc3bd77e7ac896c4a2aaa4d6c8901a5596b0bdaf
Gilbert Byiringiro <143619929+emmgJules@users.noreply.github.com>
2024-10-14 18:49:34 +0200

    last

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,15 @@
 export const signup= async (req, res) => {
-    res.json({
-        data:"You hit the signup endpoint.",
-    })
+    try {
+        const { fullName, username, email, password } = req.body;
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({error:"Invalid Email Format"})
+        }
+
+    } catch (error) {
+        
+    }
 }
 export const login= async (req, res) => {
     res.json({
--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ const app = express()
 const PORT=process.env.PORT  || '8000'
 
 dotenv.config()
+app.use(express.json())
+app.use("/api/auth", authRoutes)
 
-app.use("/api/auth",authRoutes)
 app.listen(PORT, () => {
     console.log(`server is running on port:${PORT}`)
     connectMongoDB()
